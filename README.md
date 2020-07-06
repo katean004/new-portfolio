d758703748dde269ad82a2bf474d99511d174d4a
sharky <kan004@ucr.edu>
2020-07-06 02:20:55 -0700

    removed experience section updated css and added to readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# new-portfolio
+# Portfolio
+https://katean004.github.io/new-portfolio/
+![](https://i.gyazo.com/ec9e36bdf881f1353c5ab0aa9983be90.png)
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
         $(this).toggleClass("open");
         $(".top-nav").toggleClass("open");
     });
+
+    $(".top-nav .nav-link").on("click", function(){
+        $(".menu-toggler").removeClass("open");
+        $(".top-nav").removeClass("open");
+    });
+
+    $(".go-up").on("click", function(){
+        window.scrollTo(0, 0);
+    })
+
 });
+
